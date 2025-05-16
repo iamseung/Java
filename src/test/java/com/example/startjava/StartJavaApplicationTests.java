04252ccb22ec50bf732ae817e9ee05ca2a1ec9ff
iamseung <seesungseok@naver.com>
2025-05-16 14:02:35 +0900

    first commit

---
+++ src/test/java/com/example/startjava/StartJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
